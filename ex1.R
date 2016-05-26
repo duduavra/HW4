5ceec93a85d45112c706034286810d67ff711d32
tomerwi <tomerwi@post.bgu.ac.il>
2016-05-26 21:56:07 +0300

    update

--- ex1.R
+++ ex1.R
@@ -10,110 +10,110 @@ results <- data.frame(queryId = numeric(), simTitle = numeric(),simDescription =
 library(tm)
 library(lsa)
 
-for (i in 1:10158)
-{
-  
-  #preproccesing to query
-  queries <- paste(train$query[i], collapse=" ")
-  query=NULL
-  if (length(queries)==1)
-  {
-    query=queries
-  } else {
-    queries_source <- VectorSource(queries)
-    
-    corpus <- Corpus(queries_source)
-    
-    corpus <- tm_map(corpus, content_transformer(tolower))
-    corpus <- tm_map(corpus, removePunctuation)
-    
-    corpus <- tm_map(corpus, stripWhitespace)
-    
-    corpus <- tm_map(corpus, removeWords, stopwords("english"))
-    
-    
-    dtm <- DocumentTermMatrix(corpus)
-    
-    query<-dtm$dimnames$Terms
-  }
+# for (i in 1:10158)
+# {
+#   
+#   #preproccesing to query
+#   queries <- paste(train$query[i], collapse=" ")
+#   query=NULL
+#   if (length(queries)==1)
+#   {
+#     query=queries
+#   } else {
+#     queries_source <- VectorSource(queries)
+#     
+#     corpus <- Corpus(queries_source)
+#     
+#     corpus <- tm_map(corpus, content_transformer(tolower))
+#     corpus <- tm_map(corpus, removePunctuation)
+#     
+#     corpus <- tm_map(corpus, stripWhitespace)
+#     
+#     corpus <- tm_map(corpus, removeWords, stopwords("english"))
+#     
+#     
+#     dtm <- DocumentTermMatrix(corpus)
+#     
+#     query<-dtm$dimnames$Terms
+#   }
+# 
+# 
+#   #preproccesing to title
+#   title<-paste(train$product_title[i], collapse=" ")
+# 
+#   queries_source <- VectorSource(title)
+# 
+#   corpus <- Corpus(queries_source)
+#   corpus <- tm_map(corpus, content_transformer(tolower))
+#   corpus <- tm_map(corpus, removePunctuation)
+# 
+#   corpus <- tm_map(corpus, stripWhitespace)
+# 
+#   corpus <- tm_map(corpus, removeWords, stopwords("english"))
+# 
+# 
+#   dtm <- DocumentTermMatrix(corpus)
+#   title<-dtm$dimnames$Terms
+# 
+#   #cosSimilarity between title and query
+# 
+#   write( query, file=paste(td, "D1", sep="/"))
+#   write( title, file=paste(td, "D2", sep="/"))
+#   myMatrix = textmatrix(td, minWordLength=1)
+# 
+#   similaryTitleQuery <- lsa::cosine(myMatrix[,1], myMatrix[,2])
+# 
+# 
+#   #preproccesing to description
+#   desciption<-paste(train$product_description[i], collapse=" ")
+#   d=TRUE
+#   if(desciption!=""){
+#     d=FALSE
+#   }
+# 
+#   queries_source <- VectorSource(desciption)
+# 
+#   corpus <- Corpus(queries_source)
+#   corpus <- tm_map(corpus, content_transformer(tolower))
+#   corpus <- tm_map(corpus, removePunctuation)
+# 
+#   corpus <- tm_map(corpus, stripWhitespace)
+# 
+#   corpus <- tm_map(corpus, removeWords, stopwords("english"))
+# 
+# 
+#   dtm <- DocumentTermMatrix(corpus)
+#   desciption<-dtm$dimnames$Terms
+#   desciption<-strsplit(gsub("[^[:alnum:] ]", "", desciption), " +")
+#   desciption<-as.character(desciption)
+# 
+# 
+#   write( desciption, file=paste(td, "D2", sep="/"))
+#   #totalSimilarity<-0 #initialization
+#   if(d==FALSE)
+#   {
+#     myMatrix = textmatrix(td, minWordLength=1)
+# 
+#     similaryContentQuery <- lsa::cosine(myMatrix[,1], myMatrix[,2])
+# 
+#  #   totalSimilarity<-0.8*similaryTitleQuery[1,1]+0.2*similaryContentQuery[1,1]
+# 
+#   }else {
+#  #   totalSimilarity<-similaryTitleQuery[1,1]
+#     similaryContentQuery<-0
+#   }
+# 
+#   results<-rbind(results, data.frame(queryId =train$id[i] , simTitle = similaryTitleQuery,simDescription=similaryContentQuery))
+# 
+# 
+# 
+# }
+# 
+# 
+# results["Median_Rating"] <- train$median_relevance
 
 
-  #preproccesing to title
-  title<-paste(train$product_title[i], collapse=" ")
-
-  queries_source <- VectorSource(title)
-
-  corpus <- Corpus(queries_source)
-  corpus <- tm_map(corpus, content_transformer(tolower))
-  corpus <- tm_map(corpus, removePunctuation)
-
-  corpus <- tm_map(corpus, stripWhitespace)
-
-  corpus <- tm_map(corpus, removeWords, stopwords("english"))
-
-
-  dtm <- DocumentTermMatrix(corpus)
-  title<-dtm$dimnames$Terms
-
-  #cosSimilarity between title and query
-
-  write( query, file=paste(td, "D1", sep="/"))
-  write( title, file=paste(td, "D2", sep="/"))
-  myMatrix = textmatrix(td, minWordLength=1)
-
-  similaryTitleQuery <- lsa::cosine(myMatrix[,1], myMatrix[,2])
-
-
-  #preproccesing to description
-  desciption<-paste(train$product_description[i], collapse=" ")
-  d=TRUE
-  if(desciption!=""){
-    d=FALSE
-  }
-
-  queries_source <- VectorSource(desciption)
-
-  corpus <- Corpus(queries_source)
-  corpus <- tm_map(corpus, content_transformer(tolower))
-  corpus <- tm_map(corpus, removePunctuation)
-
-  corpus <- tm_map(corpus, stripWhitespace)
-
-  corpus <- tm_map(corpus, removeWords, stopwords("english"))
-
-
-  dtm <- DocumentTermMatrix(corpus)
-  desciption<-dtm$dimnames$Terms
-  desciption<-strsplit(gsub("[^[:alnum:] ]", "", desciption), " +")
-  desciption<-as.character(desciption)
-
-
-  write( desciption, file=paste(td, "D2", sep="/"))
-  #totalSimilarity<-0 #initialization
-  if(d==FALSE)
-  {
-    myMatrix = textmatrix(td, minWordLength=1)
-
-    similaryContentQuery <- lsa::cosine(myMatrix[,1], myMatrix[,2])
-
- #   totalSimilarity<-0.8*similaryTitleQuery[1,1]+0.2*similaryContentQuery[1,1]
-
-  }else {
- #   totalSimilarity<-similaryTitleQuery[1,1]
-    similaryContentQuery<-0
-  }
-
-  results<-rbind(results, data.frame(queryId =train$id[i] , simTitle = similaryTitleQuery,simDescription=similaryContentQuery))
-
-
-
-}
-
-
-results["Median_Rating"] <- train$median_relevance
-
-
-write.csv(file="train1.csv", x=results)
+#write.csv(file="train1.csv", x=results)
 
 train1 <- read_csv("train1.csv")
 train1$ds=NULL
@@ -136,7 +136,7 @@ for (i in 1:22513)
 {
   
   #preproccesing to query
-  queries <- paste(test$query[3528], collapse=" ")
+  queries <- paste(test$query[i], collapse=" ")
   query=NULL
   if (length(queries)==1)
   {
@@ -163,7 +163,7 @@ for (i in 1:22513)
   
   
   #preproccesing to title
-  title<-paste(test$product_title[3528], collapse=" ")
+  title<-paste(test$product_title[i], collapse=" ")
   titlep=NULL
   
   if (length(queries)==1)
@@ -221,7 +221,7 @@ for (i in 1:22513)
   
   write( desciption, file=paste(td, "D2", sep="/"))
   #totalSimilarity<-0 #initialization
-  if(d==FALSE)
+  if(d==FALSE && length(desciption)!=0)
   {
     myMatrix = textmatrix(td, minWordLength=1)
     
